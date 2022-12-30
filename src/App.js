da81805a4753e4f827728ba2fa3f8558411c38fe
Nayon <nayonroy64@gmail.com>
2022-12-30 16:41:35 +0600

    project enviroment setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './Routers/Routes/Routes';
+
+function App() {
+  return <RouterProvider router={routes}>
+  </RouterProvider>;
+}
+
+export default App;
